62ffe024a101a67396db86a89cdd718a47fa9938
Vas Serg Shch <Vss@mail.com>
2023-07-02 18:44:12 +0300

    rework homework3

--- Seminar3/SeminarTask3/Program.cs
+++ Seminar3/SeminarTask3/Program.cs
@@ -1,71 +1,89 @@
-﻿// Задача 19
+﻿
+
+void PrintMessage(string a)
+{
+    Console.WriteLine(a);
+}
+
+// Задача 19
 // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-
-Console.WriteLine("введите число");
-int Number;
-Number = int.Parse(Console.ReadLine());
-
-string NumberString = Number.ToString();
-char[] CharArray = NumberString.ToCharArray();
-Array.Reverse(CharArray);
-//Console.WriteLine(CharArray);
-bool checker = false;
-int Counter = 0;
-while (Number > 100)
+void palindrome(int Number)
 {
-    Number = Number / 10;
-    if (CharArray[Counter] !=  NumberString[Counter])
+    string NumberString = Number.ToString();
+    char[] CharArray = NumberString.ToCharArray();
+    Array.Reverse(CharArray);
+    bool checker = false;
+    int Counter = 0;
+    while (Number > 100)
     {
-        checker = false;
-        break;
+        Number = Number / 10;
+        if (CharArray[Counter] !=  NumberString[Counter])
+        {
+            checker = false;
+            break;
+        }
+        else checker = true;
+        Counter++;
     }
-    else checker = true;
-    Counter++;
-}
-if (checker)
-{
-    Console.WriteLine("да");
-}
-else
-{
-    Console.WriteLine("нет");
+    if (checker)
+    {
+        Console.WriteLine("число является полиндромом");
+    }
+    else
+    {
+        Console.WriteLine("число не является полиндромом");
+    }
 }
 //Задача 21
 //Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-
-double X, Y, Z, U, V, W;
-
-Console.WriteLine("введите три числа (координаты первой точки)");
-X = double.Parse(Console.ReadLine());
-Y = double.Parse(Console.ReadLine());
-Z = double.Parse(Console.ReadLine());
-
-Console.WriteLine("введите три числа (координаты второй точки)");
-U = double.Parse(Console.ReadLine());
-V = double.Parse(Console.ReadLine());
-W = double.Parse(Console.ReadLine());
-
-double Length = Math.Sqrt((U - X) * (U - X) + (V - Y) * (V - Y) + (W - Z) * (W - Z));
-
-Console.WriteLine($"расстояние между точками =  {Length}");
-
+double CoordLength(double X, double Y, double Z, double U, double V, double W)
+{
+    double Length = Math.Sqrt(Math.Pow(U - X, 2) + Math.Pow(V - Y, 2) + Math.Pow(W - Z, 2));
+    return Length;
+}
 //Задача 23
 //Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-Console.WriteLine("введите число больше 0");
-
-int N;
-N = int.Parse(Console.ReadLine());
-int PowN = 0;
-
-if (N > 0)
+void CubeNumbers(int N)
 {
-    for (int i = 1; i <= N; i++)
+    int PowN = 0;
+    if (N > 0)
     {
-        Console.WriteLine(Math.Pow(i, 3));
+        for (int i = 1; i <= N; i++)
+        {
+            Console.WriteLine(Math.Pow(i, 3));
+     }
+    }
+    else 
+    {
+        Console.WriteLine("некорректное число");
     }
 }
-else 
-{
-    Console.WriteLine("некорректное число");
-}
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+PrintMessage("введите число");
+int Value = int.Parse(Console.ReadLine());
+palindrome(Value);
+
+//Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+double A, B, C, D, E, F, L = 0;
+PrintMessage("введите три числа (координаты первой точки)");
+A = double.Parse(Console.ReadLine());
+B = double.Parse(Console.ReadLine());
+C = double.Parse(Console.ReadLine());
+PrintMessage("введите три числа (координаты второй точки)");
+D = double.Parse(Console.ReadLine());
+E = double.Parse(Console.ReadLine());
+F = double.Parse(Console.ReadLine());
+L = CoordLength(A, B, C, D, E, F);
+Console.WriteLine($"расстояние между точками =  {L}");
+
+//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+PrintMessage("введите число больше 0");
+Value = int.Parse(Console.ReadLine());
+CubeNumbers(Value);
