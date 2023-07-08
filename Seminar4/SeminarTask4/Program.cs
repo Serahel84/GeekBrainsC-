51053da7bf87ac579f6f94835282addff198ef14
Vas Serg Shch <Vss@mail.com>
2023-07-08 22:40:25 +0300

    Add HomeTask 4 and 5

---
+++ Seminar4/SeminarTask4/Program.cs
@@ -0,0 +1,66 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int PowFoo(int a, int b)
+{
+    int A = a;
+    for (int i = 1; i < b; i++)
+    {
+        A *= a;
+    }   
+    return A;
+}
+
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int SumOfNumbers(int a)
+{
+    int A = 0;
+    while (a > 0)
+    {
+        A += a % 10;
+        a /= 10;
+    }
+    return A;
+}
+
+//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void SetRandArray(int[] array)
+{
+    Random X = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = X.Next(0, 100);
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+
+//--------------------------------------------------
+
+//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int a, b;
+Console.WriteLine("введите два числа");
+a = int.Parse(Console.ReadLine());
+b = int.Parse(Console.ReadLine());
+int A = PowFoo(a, b);
+Console.WriteLine($" первое число в степени второго числа = {A}");
+
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("введите число");
+a = int.Parse(Console.ReadLine());
+A = SumOfNumbers(a);
+Console.WriteLine($" сумма всех цифр в числе = {A}");
+
+//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] Array = new int[8];
+SetRandArray(Array);
+PrintArray(Array);
---
+++ Seminar5/SeminarTask5/Program.cs
@@ -0,0 +1,116 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void SetPositRandArray(int[] array)
+{
+    Random X = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = X.Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+
+void PrintPositNumArr(int[] array)
+{
+    int Counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            Counter++;
+        }
+    }
+    Console.WriteLine($"четных чисел в массиве {Counter}");
+}
+
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+void SetRandArray(int[] array)
+{
+    Random X = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = X.Next(-99, 100);
+    }
+}
+
+int GetSumOfOddPos(int[] array)
+{
+    int Summ = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            Summ += array[i];
+        }
+    }
+    return Summ;
+}
+
+//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+void SetDoubleRandArray(double[] array)
+{
+    Random X = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = X.NextDouble() * (100.0 + 100.0) - 100.0;
+    }
+}
+
+void PrintDArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(Math.Round(array[i], 2));
+    }
+}
+
+double GetDiffMinMax(double[] array)
+{
+    double min = array[0], max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        if (array[i] < min)
+        {
+             min = array[i];
+        }
+    }
+    return max - min;
+}
+
+//--------------------------------------------------------------------------------
+
+//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int Size = 6;
+int[] Array = new int[Size];
+SetPositRandArray(Array);
+PrintArray(Array);
+PrintPositNumArr(Array);
+
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+SetRandArray(Array);
+PrintArray(Array);
+int SummOddPos = GetSumOfOddPos(Array);
+Console.WriteLine($"сумму элементов, стоящих на нечётных позициях = {SummOddPos}");
+
+//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] DArray = new double[Size];
+SetDoubleRandArray(DArray);
+PrintDArray(DArray);
+double Difference = GetDiffMinMax(DArray);
+Console.WriteLine($"разницу между максимальным и минимальным элементов массива = {Math.Abs(Math.Round(Difference, 2))}");
